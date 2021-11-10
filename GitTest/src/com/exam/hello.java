99f3b8659d2ecd3d64d388ab0567a09e07dc044b
smhrd <smhrd@59.0.236.242>
2021-11-10 16:56:22 +0900

    여왕벌과 바보들

--- GitTest/src/com/exam/hello.java
+++ GitTest/src/com/exam/hello.java
@@ -3,8 +3,14 @@ package com.exam;
 public class hello {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("형지최고 '_<");
+		System.out.println("강명재바보");
+		System.out.println("박현규바보");
+		System.out.println("이민솔바보");
+		System.out.println("이민수바보");
 
+		System.out.println("무야호`~~~~~");
 	}
 
 }
