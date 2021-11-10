99b00d5be52f2ae0e8c695b06a63eae1c4f40d3e
smhrd <smhrd@59.0.236.33>
2021-11-10 15:40:15 +0900

    두 번째 커밋!

--- GitTest/src/com/exam/Test01.java
+++ GitTest/src/com/exam/Test01.java
@@ -5,6 +5,7 @@ public class Test01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("첫 커밋!");
+		System.out.println("두 번째 커밋! 카페 어디까지 가봤니?");
 	}
 
 }
