e0fe57f604ef7225e917ceb3ca940d3e1b1172f9
smhrd <smhrd@59.0.236.33>
2021-11-10 16:19:12 +0900

    세 번째 커밋

--- GitTest/src/com/exam/Test02.java
+++ GitTest/src/com/exam/Test02.java
@@ -3,7 +3,9 @@ package com.exam;
 public class Test02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stubs
+		System.out.println("세 번째 커밋! 카페 어디까지 가봤니?");
+		
 
 	}
 
