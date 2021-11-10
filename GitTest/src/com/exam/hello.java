1afd73a08a814c3b37f1dba78a457e8bc4978e59
smhrd <smhrd@59.0.129.66>
2021-11-10 17:21:41 +0900

    깃 공유테스트중 박현규바보->박현규짱짱맨

--- GitTest/src/com/exam/hello.java
+++ GitTest/src/com/exam/hello.java
@@ -6,14 +6,10 @@ public class hello {
 		
 		System.out.println("형지최고 '_<");
 		System.out.println("강명재바보");
-		System.out.println("박현규바보");
-<<<<<<< HEAD
+		System.out.println("박현규짱짱맨");
 		System.out.println("이민솔");
-		System.out.println("이민수바보");
-=======
-		System.out.println("이민솔바보");
 		System.out.println("이민수는 바보 아님");
->>>>>>> branch 'master' of https://github.com/2021-SMHRD-KDT-AI-7/CafePlease.git
+
 
 		System.out.println("무야호`~~~~~");
 	}
