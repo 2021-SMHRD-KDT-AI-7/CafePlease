42a7b2416e093eafc908e2ece6642120d03197ca
smhrd <smhrd@59.0.236.242>
2021-11-23 19:22:49 +0900

    홈,게시판 왼쪽정렬된거 css수정 cafe_info관련 dao,servicecon 생성

--- GitTest/src/Controller/CafeInfoServiceCon.java
+++ GitTest/src/Controller/CafeInfoServiceCon.java
@@ -20,25 +20,25 @@ import Model.MemberDAO;
 
 @WebServlet("/CafeInfoServiceCon")
 public class CafeInfoServiceCon extends HttpServlet {
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		
-	    System.out.println("카페 정보띄워라"); 
-	
-		request.setCharacterEncoding("EUC-KR");
-		String 이미지=request.getParameter("이미지"); //클릭시 이미지 파일 경로가 와야함
-		
-	    //<a href="CafeInfoServiceCon? 경로 적기 이거로 보내야 받을수 있음. 쿼리스트링 사용!
-		//something은 받아올 이미지 경로!
-	
-		CafeDAO dao= new CafeDAO();
-		
-		CafeDTO inf= dao.cafeimg_info(이미지);
-		
-		if (inf != null) {
-			HttpSession session = request.getSession();
-			session.setAttribute("inf", inf);
-			response.sendRedirect("돌아가야할 카페정보 페이지");}
-	}
+   protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      
+      
+       System.out.println("카페 정보띄워라"); 
+   
+      request.setCharacterEncoding("EUC-KR");
+      String 이미지=request.getParameter("이미지"); //클릭시 이미지 파일 경로가 와야함
+      
+       //<a href="CafeInfoServiceCon? 경로 적기 이거로 보내야 받을수 있음. 쿼리스트링 사용!
+      //something은 받아올 이미지 경로!
+   
+      CafeDAO dao= new CafeDAO();
+      
+      CafeDTO inf= dao.cafeimg_info(이미지);
+      
+      if (inf != null) {
+         HttpSession session = request.getSession();
+         session.setAttribute("inf", inf);
+         response.sendRedirect("돌아가야할 카페정보 페이지");}
+   }
 
 }
